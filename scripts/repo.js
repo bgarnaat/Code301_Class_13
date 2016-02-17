be4c3448b2c8334bfebdc616b224126db21142fd
Ben Garnaat <garnaat.ben@gmail.com>
2016-02-17 15:26:48 -0800

    everything works

--- scripts/repo.js
+++ scripts/repo.js
@@ -5,17 +5,14 @@
 
   // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/brookr/repos' +
+    $.get('/github/users/brookr/repos' +
             '?per_page=100' +
             '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + process.env.GITHUB_TOKEN },
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
-    }).done(callback);
+          function(data, message, xhr){
+            repos.all = data;
+          }).done(callback);
   };
+
 
   repos.with = function(attr) {
     return repos.all.filter(function(repo) {
